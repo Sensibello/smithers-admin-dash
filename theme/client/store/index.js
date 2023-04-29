2bf39f3c150360baaeb161ad107828314d63e752
jdhofmann <justindhofmann@gmail.com>
2023-04-29 10:04:31 -0400

    removed theme/index.js from store - seems to work without

--- theme/client/store/index.js
+++ theme/client/store/index.js
@@ -1,75 +1,75 @@
-import { makeAuthPlugin, initAuth, models } from '~/plugins/feathers'
+import { makeAuthPlugin, initAuth, models } from "~/plugins/feathers";
 const auth = makeAuthPlugin({
-  userService: 'users',
+  userService: "users",
   state: {
-    publicPages: [
-      'frontend-user-login',
-      'frontend'
-    ]
+    publicPages: ["frontend-user-login", "frontend"],
   },
   actions: {
-    onInitAuth ({ state, dispatch }, payload) {
+    onInitAuth({ state, dispatch }, payload) {
       if (payload) {
-        dispatch('authenticate', { strategy: 'jwt', accessToken: state.accessToken })
+        dispatch("authenticate", {
+          strategy: "jwt",
+          accessToken: state.accessToken,
+        })
           .then((result) => {
             // handle success like a boss
-            console.log('loged in')
+            console.log("logged in");
           })
           .catch((error) => {
             // handle error like a boss
-            console.log(error)
-          })
+            console.log(error);
+          });
       }
-    }
-  }
-})
+    },
+  },
+});
 
 const requireServices = require.context(
   // The path where the service modules live
-  './services',
+  "./services",
   // Whether to look in subfolders
   true,
   // Only include .js files (prevents duplicate imports`)
   /.js$/
-)
+);
 const servicePlugins = requireServices
   .keys()
-  .map(modulePath => requireServices(modulePath).default)
+  .map((modulePath) => requireServices(modulePath).default);
 
 export const modules = {
   // ...customModules
-}
+};
 
 export const state = () => ({
   // Custom state
-})
+});
 
 export const mutations = {
   // Custom mutations
-}
+};
 
 export const actions = {
   // Custom actions
-  nuxtServerInit ({ commit, dispatch }, { req }) {
+  nuxtServerInit({ commit, dispatch }, { req }) {
     return initAuth({
       commit,
       dispatch,
       req,
-      moduleName: 'auth',
-      cookieName: 'feathers-jwt'
-    })
+      moduleName: "auth",
+      cookieName: "feathers-jwt",
+    });
   },
-  nuxtClientInit ({ state, dispatch, commit }, context) {
+  nuxtClientInit({ state, dispatch, commit }, context) {
     // hydrateApi({ api: models.api })
 
     if (state.auth.accessToken) {
-      return dispatch('auth/onInitAuth', state.auth.payload)
+      return dispatch("auth/onInitAuth", state.auth.payload);
     }
-  }
-}
+  },
+};
 
 export const getters = {
   // Custom getters
-}
+};
 
-export const plugins = [ ...servicePlugins, auth ]
+export const plugins = [...servicePlugins, auth];
